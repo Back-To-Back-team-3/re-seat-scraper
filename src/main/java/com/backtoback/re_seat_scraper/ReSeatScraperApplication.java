8b7cc600a628fb0065d839e5470b2418352e6453
R1NN <r1nn650t@gmail.com>
2026-07-04 12:38:45 +0900

    chore: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/backtoback/re_seat_scraper/ReSeatScraperApplication.java
@@ -0,0 +1,13 @@
+package com.backtoback.re_seat_scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReSeatScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReSeatScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backtoback/re_seat_scraper/ReSeatScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backtoback.re_seat_scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReSeatScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
